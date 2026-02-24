74a79a3155397957cbebcc7d02195f8688aa0a0e
Gitman009 <137716237+Gitman009@users.noreply.github.com>
2026-02-24 14:43:56 +0530

    bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import requests
+from datetime import datetime
+
+TOKEN = "7982592552:AAHebslaeHfca3dUpyPBX0_TLw_HwwGi5bk"
+CHAT_ID = "1039438785"
+
+message = """ 📈 Bot Capture A Intraday Stock
+📅 {datetime.now().strftime('%d-%m-%Y')}
+
+📈 Tomorrow Intraday Pick
+Stock: RELIANCE
+Signal: BUY
+
+Strategy:
+Basic Test Signal
+"""Basic Test Signal
+VWAP Support + RSI Strength
+MACD Bullish Cross
+Volume Expansion Seen
+Momentum Intact
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+data = {"chat_id": CHAT_ID, "text": message}
+requests.post(url, data=data)
